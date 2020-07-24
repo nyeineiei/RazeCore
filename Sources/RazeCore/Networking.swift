48a590c75f92f3775ffc7ad30441706c560130c8
Nyein Ei Ei Tun <pauksohne2t@gmail.com>
2020-07-24 11:39:45 +0800

    Added dependency injection to Network manager

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -7,27 +7,45 @@
 
 import Foundation
 
+protocol NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: url) { data, _, error in
+            completionHandler(data,error)
+        }
+        task.resume()
+    }
+    
+}
+
 extension RazeCore {
     public class Networking {
         /// Responsible for handling all networking calls
         /// - Warning: Must create before using any public APIs
         public class Manager {
-          private let session = URLSession.shared
-          
-          public init() {}
-          
-          public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
-            let task = session.dataTask(with: url) { data, response, error in
-              let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
-              completionHandler(result)
+            internal var session: NetworkSession = URLSession.shared
+            
+            public init() {}
+            
+            
+            /// Call to the live internet to retrieve data from specific location
+            /// - Parameters:
+            ///   - url: The location you wish to fetcht the data from
+            ///   - completionHandler: Return the result which signifies the status of the request
+            public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+                session.get(from: url) { (data, error) in
+                    let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                    completionHandler(result)
+                }
             }
-            task.resume()
-          }
-          
-          public enum NetworkResult<Value> {
-            case success(Value)
-            case failure(Error?)
-          }
+            
+            public enum NetworkResult<Value> {
+                case success(Value)
+                case failure(Error?)
+            }
         }
     }
 }
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -8,28 +8,41 @@
 import XCTest
 @testable import RazeCore
 
-class RazeNetworkingTests: XCTestCase {
+class NetworkSessionMock: NetworkSession {
+    var data: Data?
+    var error: Error?
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+}
 
+class RazeNetworkingTests: XCTestCase {
+    
     func testLoadDataCall() {
-      let manager = RazeCore.Networking.Manager()
-      let expectation = XCTestExpectation(description: "Called for data")
-      guard let url = URL(string: "https://raywenderlich.com") else {
-        return XCTFail("Could not create URL properly")
-      }
-      manager.loadData(from: url) { result in
-        expectation.fulfill()
-        switch result {
-          case .success(let returnedData):
-            XCTAssertNotNil(returnedData, "Response data is nil")
-          case .failure(let error):
-            XCTFail(error?.localizedDescription ?? "error forming error result")
+        let manager = RazeCore.Networking.Manager()
+        let session = NetworkSessionMock()
+        manager.session = session
+        
+        let expectation = XCTestExpectation(description: "Called for data")
+        
+        let data = Data([0, 1, 0, 1])
+        session.data = data
+        let url = URL(fileURLWithPath: "url")
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                XCTAssertEqual(data, returnedData, "Manager returned unexpected data")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
         }
-      }
-      wait(for: [expectation], timeout: 5)
+        wait(for: [expectation], timeout: 5)
     }
     
     static var allTests = [
-      ("testLoadDataCall", testLoadDataCall)
+        ("testLoadDataCall", testLoadDataCall)
     ]
-
+    
 }
